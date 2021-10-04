cc8b6b0d3e8896b4c03c1b236b3e93d8dafaa39f
ShaneWarga <54457686+ShaneWarga@users.noreply.github.com>
2021-10-04 17:22:54 -0500

    New clean branch, install and update dependencies to get going

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import RunTable from './Components/RunTable'
+import NavBar from './Components/NavBar';
 function App() {
   return (
-    <div className="AppBar">
-      <RunTable apiPath = "https://data.washuracing.com/api/v2/testing"></RunTable>     
+    <div className="App">
+      {/* <div className="AppBar">
+        <NavBar />
+      </div> */}
+      <RunTable apiPath = "https://data.washuracing.com/api/v2/testing"></RunTable>
+      <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />     
     </div>
   );
 }
